773b1abf74decb5fa8c55871abf71b1aa7fcfc7f
Reketino <bjornevensk8@gmail.com>
2025-11-04 20:22:31 +0100

    update

--- src/app/page.js
+++ src/app/page.js
@@ -14,13 +14,14 @@ export default function brainstormpage() {
           <p className="text-lg mb-4">Maybe the book is on a computer screen</p>
           <p className="text-lg mb-4">First Page about me</p>
           <p className="text-lg mb-4">Second Page Projects</p>
-          <p className="text-lg mb-4">Third Page Contact</p>
+          <p className="text-lg mb-4">Third Page Skills</p>
+          <p className="text-lg mb-4">Fourth Page Contact</p>
           <p className="text-lg mb-4">Footer with social media links</p>
         </div>
         <div className="flex flex-col items-center justify-center mt-4">
           <p className="text-lg mb-4 font-semibold">Idea 2</p>
           <p className="text-lg mb-4">Computer Screen showcasing My Name as a background and about me</p>
-          <p className="text-lg mb-4"> Different icons for contact, projects icons etc.</p>
+          <p className="text-lg mb-4"> Different icons for contact, projects, skills with icons</p>
         </div>
     </main>
   )
